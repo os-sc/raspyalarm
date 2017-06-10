821643f68178c996b3f336eac7854a9cb24bcbe3
osiris <srgv61@gmail,com>
2017-06-11 00:06:13 +0200

    Added actions to event

--- alarmconfig/events/event.py
+++ alarmconfig/events/event.py
@@ -5,7 +5,14 @@ class Event():
         if not name:
             raise ValueError('Name cannot be empty for Event')
         self.name = name
+        self._actions = []
         self._time = None
+
+    def execute_actions(self):
+        results = []
+        for action in self._actions:
+            results.append(action.execute())
+        return results
 
     def setTime(self, time):
         self._time = TimeString(time)
